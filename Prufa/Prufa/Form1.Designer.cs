63e06987ee6b891c5f45302b50a0434a3018966b
kari12345 <karikon1@gmail.com>
2016-11-23 20:21:38 +0000

    ff

--- Prufa/Prufa/Form1.Designer.cs
+++ Prufa/Prufa/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 54);
             this.button1.TabIndex = 0;
-            this.button1.Text = "oli";
+            this.button1.Text = "gulli";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
